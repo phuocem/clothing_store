b04ce23342343c497abf52acf70085b12b3408bb
p.anh <phuocanh554@gmail.com>
2024-10-31 10:21:12 +0700

    update navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,12 +9,34 @@ import { Router } from '@angular/router';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
-  constructor(private router: Router) {} // Inject Router
+  isMenuOpen = false; // Biến để theo dõi trạng thái của menu
 
+  // Constructor nhận Router và Renderer2
+  constructor(private router: Router, private renderer: Renderer2) {}
+
+  // Hàm để điều hướng đến trang đăng nhập
   activateRightPanel() {
-    this.router.navigate(['/login']); // Navigate to the login page
+    this.router.navigate(['/login']); // Điều hướng đến trang đăng nhập
   }
-  home(){
-    this.router.navigate(['']);
+
+  // Hàm để điều hướng về trang chủ
+  home() {
+    this.router.navigate(['']); // Điều hướng về trang chủ
+  }
+
+  // Hàm để mở/đóng menu
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+    if (this.isMenuOpen) {
+      this.renderer.setStyle(document.body, 'overflow', 'hidden'); // Khóa cuộn trang
+    } else {
+      this.renderer.setStyle(document.body, 'overflow', ''); // Kích hoạt cuộn trang
+    }
+  }
+
+  // Hàm để đóng menu
+  closeMenu() {
+    this.isMenuOpen = false; // Đặt trạng thái menu thành đóng
+    this.renderer.setStyle(document.body, 'overflow', ''); // Kích hoạt cuộn trang
   }
 }
